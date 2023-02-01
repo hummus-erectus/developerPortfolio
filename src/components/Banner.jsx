e75f6589269310346aff0ea1697e620d06156b05
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-02-01 14:27:12 +0900

    Added react-scroll

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,7 +2,7 @@ import runner from '../assets/runner.webp'
 
 function Banner() {
     return (
-        <section className="mt-20 scroll-mt-[96px] md:mt-24 mx-auto w-[95%] md:w-auto container grid items-center rounded-[50px] bg-neutral md:grid-cols-2 p-8">
+        <section id='home' className="mt-20 scroll-mt-[96px] md:mt-24 mx-auto w-[95%] md:w-auto container grid items-center rounded-[50px] bg-neutral md:grid-cols-2 p-8">
             <div className="max-w-[640px]  text-center md:text-left items-center justify-center">
                 <h1 className="mb-5 text-3xl md:text-5xl lg:text-6xl  font-bold">Japan-based frontend developer</h1>
                 <h2 className="mb-5 text-xl md:text-3xl lg:text-4xl text-[#1EB854] font-bold ">Robert Grayson</h2>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react'
+import * as Scroll from 'react-scroll';
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 import resume from '../files/resume.pdf'
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
+// import { FaGithub, FaLinkedin } from 'react-icons/fa'
 
 function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false)
-    const [activeLink, setActiveLink] = useState ('home')
 
     useEffect(() => {
         const handleClick = (e) => {
@@ -16,41 +17,41 @@ function Navbar() {
         return () => document.removeEventListener('click', handleClick)
     }, [])
 
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
-    }
+
 
     return (
         <nav className='navbar fixed top-0 bg-neutral z-50'>
             <div className="navbar-start">
                 <div className="dropdown">
-                    <button className="btn md:hidden" onClick={() => isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true) }>
+                    <label tabIndex={0} className="btn md:hidden" onClick={() => isMenuOpen ? setIsMenuOpen(false) : setIsMenuOpen(true) }>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                    </button>
-                    <div className={`menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral rounded-box w-52 ${isMenuOpen ? 'block' : 'hidden'}`}>
-                        <ul onClick={() => setIsMenuOpen(false)}>
-                            <li><a href="#about" onClick={() => onUpdateActiveLink('about')}>About</a></li>
-                            <li><a href="#skills"  onClick={() => onUpdateActiveLink('skills')}>Skills</a></li>
-                            <li><a href="#projects"  onClick={() => onUpdateActiveLink('projects')}>Projects</a></li>
-                            <li><a href="#contact"  onClick={() => onUpdateActiveLink('contact')}>Contact</a></li>
+                    </label>
+                    <div tabIndex={0} className={`menu menu-compact dropdown-content mt-3 p-2 shadow bg-neutral rounded-box w-52 ${isMenuOpen ? 'block' : 'hidden'}`}>
+                        <ul>
+                            <li><Link activeClass="active" spy={true} smooth={true} offset={-96} to="home">Home</Link></li>
+                            <li><Link activeClass="active" spy={true} smooth={true} offset={-96} to="about">About</Link></li>
+                            <li><Link activeClass="active" spy={true} smooth={true} offset={-94} to="skills">Skills</Link></li>
+                            <li><Link activeClass="active" spy={true} smooth={true} offset={-94} to="projects">Projects</Link></li>
+                            <li><Link activeClass="active" spy={true} smooth={true} offset={-94} to="contact">Contact</Link></li>
                         </ul>
                         <hr/>
-                        <ul onClick={() => setIsMenuOpen(false)}>
+                        <ul >
                             <li><a href='https://www.linkedin.com/in/rob-grayson/' target="_blank">LinkedIn</a></li>
                             <li><a href='https://github.com/hummus-erectus' target="_blank">GitHub</a></li>
                         </ul>
                     </div>
                 </div>
-                <a href="#"className="btn  normal-case text-xl" onClick={() => onUpdateActiveLink('')}>Robert Grayson</a>
+                <Link className="btn normal-case text-xl" spy={true} smooth={true} offset={-96} to="home">Robert Grayson</Link>
             </div>
             <div className="navbar-center hidden md:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a href="#about" className={activeLink === 'about' ? 'btn-secondary text-neutral' : 'undefined'} onClick={() => onUpdateActiveLink('about')}>About</a></li>
-                    <li><a href="#skills"  className={activeLink === 'skills' ? 'btn-secondary text-neutral' : 'undefined'} onClick={() => onUpdateActiveLink('skills')}>Skills</a></li>
-                    <li><a href="#projects" className={activeLink === 'projects' ? 'btn-secondary text-neutral' : 'undefined'} onClick={() => onUpdateActiveLink('projects')}>Projects</a></li>
-                    <li><a href="#contact" className={activeLink === 'contact' ? 'btn-secondary text-neutral' : 'undefined'} onClick={() => onUpdateActiveLink('contact')}>Contact</a></li>
-                    <li><a href='https://www.linkedin.com/in/rob-grayson/' target="_blank" className='ml-2 lg:ml-10'><FaLinkedin /></a></li>
-                    <li><a href='https://github.com/hummus-erectus' target="_blank"><FaGithub /></a></li>
+                    <li><Link activeClass="active" spy={true} smooth={true} offset={-96} to="home">Home</Link></li>
+                    <li><Link activeClass="active" spy={true} smooth={true} offset={-96} to="about">About</Link></li>
+                    <li><Link activeClass="active" spy={true} smooth={true} offset={-94} to="skills">Skills</Link></li>
+                    <li><Link activeClass="active" spy={true} smooth={true} offset={-94} to="projects">Projects</Link></li>
+                    <li><Link activeClass="active" spy={true} smooth={true} offset={-94} to="contact">Contact</Link></li>
+                    {/* <li><a href='https://www.linkedin.com/in/rob-grayson/' target="_blank" className='ml-2 lg:ml-10'><FaLinkedin /></a></li>
+                    <li><a href='https://github.com/hummus-erectus' target="_blank"><FaGithub /></a></li> */}
                 </ul>
             </div>
             <div className="navbar-end">
